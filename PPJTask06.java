46e7e289074b732d9bfbf745941725772788fbee
Lucas Campbell <lucasc8558@hotmail.com>
2019-12-02 23:15:00 +0100

    ?

--- PPJTask06.java
+++ PPJTask06.java
@@ -8,10 +8,14 @@ public class PPJTask06{
 	            { {100, -50, 25}, {150,-300}, {300,-90,100} },
 	            { {90, -60, 250}, {300,20,-100} },
 	            { {20, 50}, {300}, {20,-20,40}, {100,-200} }
-	};
+			};
+		
+		for(int[][] a : opers)
+			System.out.print(Arrays.deepToString(a));
+		
+	}
 
-		System.out.println(Arrays.deepToString(opers));
-
-  }
-
+	
+	
+	
 }
