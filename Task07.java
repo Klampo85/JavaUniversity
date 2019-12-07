86789801514a764ccd7bf6ada483c849147b1661
Lucas Campbell <lucasc8558@hotmail.com>
2019-12-07 16:53:51 +0100

    Added placeholders, fixed error

--- Task07.java
+++ Task07.java
@@ -1,15 +1,32 @@
+import java.lang.management.*;
 
-public class Task07{
+public class Task07 {
 
     public static void main(String[] args){
 
         
+
     }
 
+    public static String norm(String name){
+
+        String placeholder = "Hello";
 
 
+        return placeholder;
+    }
 
+    public static String init(String name){
 
+        String placeholder = "Hello";
 
+        return placeholder;
+    }
 
+    public static String tr(String s, String from, String to){
+
+        String placeholder = "Hello";
+
+        return placeholder;
+    }
 }
