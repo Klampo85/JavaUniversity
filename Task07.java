744825a3342d977fd0dbc3b4adea20a86f182ef5
Lucas Campbell <lucasc8558@hotmail.com>
2019-12-11 19:21:13 +0100

    Updated homework

--- Task07.java
+++ Task07.java
@@ -1,19 +1,15 @@
-import java.lang.management.*;
 
 public class Task07 {
 
     public static void main(String[] args){
-
-        
+    	
+        System.out.println(norm("campbell"));
 
     }
 
     public static String norm(String name){
 
-        String placeholder = "Hello";
-
-
-        return placeholder;
+        return name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
     }
 
     public static String init(String name){
